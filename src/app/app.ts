aa74ebbb39943c8877d8a07cff2635ce02c8be45
cwins <109783695+cwins@users.noreply.github.com>
2026-03-16 10:49:39 -0500

    fix: tiny fixes to button and example usage

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Button } from "../components";
-import { KeyValuePipe, NgClass } from "@angular/common";
+import { KeyValuePipe } from "@angular/common";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Button, NgClass, KeyValuePipe],
+  imports: [RouterOutlet, Button, KeyValuePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
